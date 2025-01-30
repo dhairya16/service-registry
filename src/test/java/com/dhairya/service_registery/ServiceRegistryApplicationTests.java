61b5e2d83e7aeefa8d362e90eb6ed65657bc735d
Dhairya Savlekar <dhairyasavlekar16@gmail.com>
2025-01-30 19:30:18 +0530

    first commit

---
+++ src/test/java/com/dhairya/service_registery/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhairya.service_registery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
